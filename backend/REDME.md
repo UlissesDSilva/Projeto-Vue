a9e4b5962250ca05d367987a72e3dfcfd4863656
Ulisses Queiroz da Silva <62977318+UlissesDSilva@users.noreply.github.com>
2026-06-22 19:02:01 -0300

    Redme

---
+++ backend/REDME.md
@@ -0,0 +1,12 @@
+Projeto tem como intuito forma uma báse de conhecimento sobre backend, usando node como tecnologia por trás 
+
+### Knex
+-- Biblioteca usada para acessos de bancos relacionais - Query builder
+Migration - comandos
+  - knex init
+  - knex migrate:make name_migrate
+  - knex migrate:latest
+  - Knex migrate:rollback
+
+### Consign
+-- Ajuda definir as dependencias dentro a aplicação
--- backend/api/category.js
+++ backend/api/category.js
@@ -56,7 +56,7 @@ module.exports = app => {
 
   const withPath = categories => {
     const getParent = (categories, parentId) => {
-      const  parent = categories.filter((parent) => parent.id === parentId)
+      const parent = categories.filter((parent) => parent.id === parentId)
       return parent.length ? parent[0] : null
     }
 
@@ -73,8 +73,8 @@ module.exports = app => {
     })
 
     categoriesWithPath.sort((a, b) => {
-      if (a.path > b.path) return -1
-      if (a.path < b.path) return 1
+      if (a.path < b.path) return -1
+      if (a.path > b.path) return 1
       return 0
     })
 
